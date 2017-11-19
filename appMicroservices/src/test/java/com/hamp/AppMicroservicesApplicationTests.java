fe50aff17059cf3ede2dfe5ab6c3d7cd164c1ad2
Henry A. Mendoza Puerta <hmendo81@gmail.com>
2017-11-18 20:09:59 -0500

    Demo IC Jenkins

---
+++ appMicroservices/src/test/java/com/hamp/AppMicroservicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hamp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppMicroservicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
