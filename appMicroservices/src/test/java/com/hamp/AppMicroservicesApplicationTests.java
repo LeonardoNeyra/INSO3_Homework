947a9d7bf2378372c1cc2866c5f0271d41919d0a
Henry A. Mendoza Puerta <hmendo81@gmail.com>
2017-11-19 10:45:13 -0500

    Test

--- appMicroservices/src/test/java/com/hamp/AppMicroservicesApplicationTests.java
+++ appMicroservices/src/test/java/com/hamp/AppMicroservicesApplicationTests.java
@@ -1,16 +1,30 @@
 package com.hamp;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.hamp.model.Persona;
+import com.hamp.repository.IPersonaRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppMicroservicesApplicationTests {
 
+	@Autowired
+	IPersonaRepository ipersonar;
+	
 	@Test
 	public void contextLoads() {
+	
+		Persona p=new Persona();
+		p.setCorreo("hmendo81@gmail.com");
+		assertEquals("1", p);
+		
 	}
 
 }
